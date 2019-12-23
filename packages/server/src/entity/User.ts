00e4566d4e8ac3aac472b6d81a0899e564956377
ironsoul0 <timka2609@gmail.com>
2019-12-24 01:50:29 +0600

    Logic for updating users submissions

--- packages/server/src/entity/User.ts
+++ packages/server/src/entity/User.ts
@@ -29,7 +29,7 @@ export class User extends BaseEntity {
   @Column("text")
   password: string;
 
-  @Column("bigint", { nullable: true })
+  @Column({ nullable: true })
   lastCheckedSubmission?: number;
 
   @Field(() => [Ladder])
--- packages/server/src/utils/codeforces.ts
+++ packages/server/src/utils/codeforces.ts
@@ -3,6 +3,15 @@ import axios from "axios";
 import { User } from "../entity/User";
 import { Problem } from "../entity/Problem";
 
+interface Submission {
+  creationTimeSeconds: number;
+  problem: {
+    contestId: number;
+    index: string;
+  };
+  verdict: string;
+}
+
 const API = "https://codeforces.com/api";
 
 export const PROBLEM_URL = "https://codeforces.com/problemset/problem";
@@ -16,47 +25,66 @@ export const doesHandleExist = async (handle: string): Promise<boolean> => {
   }
 };
 
-const hasMatch = (
-  contestId: number,
-  problemIndex: string,
-  problemEndpoints: string[]
-) => {
-  return problemEndpoints.some(
-    endpoint => endpoint === `${contestId}/${problemIndex}`
+const hasMatch = (problem: Problem, submission: Submission) => {
+  const { contestId, index } = submission.problem;
+  return problem.endpoints.some(
+    endpoint => endpoint === `${contestId}/${index}`
   );
-};
-
-export const checkSolved = async (
-  handle: string,
-  problemEndpoints: string[]
-): Promise<boolean> => {
-  try {
-    const status = await axios.get(`${API}/user.status?handle=${handle}`);
-    const submissions = status.data.result;
-    return submissions.some((submission: any) => {
-      const { problem, verdict } = submission;
-      return (
-        hasMatch(problem.contestId, problem.index, problemEndpoints) &&
-        verdict === "OK"
-      );
-    });
-  } catch {
-    return false;
-  }
 };
 
 export const updateSubmissions = async (user: User) => {
   const problems = await Problem.find();
   const { lastCheckedSubmission, handle } = user;
 
-  const problemsExist = true;
-  const limit = 5;
-  const offset = 1;
+  console.log(lastCheckedSubmission);
 
-  while (problemsExist) {
-    const status = await axios.get(`${API}/user.status?handle=${handle}`);
-    const submissions;
+  const limit = 10;
+
+  let newProblemsExist = true;
+  let offset = 1,
+    newCheckedSubmission = null;
+
+  const newSolved: Submission[] = [];
+
+  while (newProblemsExist) {
+    const QUERY_URL =
+      `${API}/user.status?handle=${handle}&from=${offset}` +
+      (lastCheckedSubmission ? `&count=${limit}` : "");
+
+    const status = await axios.get(QUERY_URL);
+
+    const submissions = status.data.result;
+
+    if (submissions.length === 0) {
+      newProblemsExist = false;
+    }
+
+    if (newCheckedSubmission === null && submissions.length > 0) {
+      newCheckedSubmission = submissions[0].creationTimeSeconds;
+    }
+
+    submissions.forEach((submission: Submission) => {
+      if (submission.creationTimeSeconds == lastCheckedSubmission) {
+        newProblemsExist = false;
+      }
+      if (!newProblemsExist || submission.verdict !== "OK") return;
+      newSolved.push(submission);
+    });
+
+    offset += submissions.length;
   }
 
-  return true;
+  const promises = newSolved.map(async submission => {
+    const ladderProblem = problems.filter(problem =>
+      hasMatch(problem, submission)
+    );
+    if (ladderProblem.length > 0) {
+      user.problems.push(ladderProblem[0]);
+    }
+  });
+
+  user.lastCheckedSubmission = newCheckedSubmission;
+
+  await Promise.all(promises);
+  await user.save();
 };
