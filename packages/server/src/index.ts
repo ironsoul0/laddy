bb31cb5cea3672a4f47e4cf1f0ff285a1ec5b7f2
ironsoul0 <timka2609@gmail.com>
2020-01-05 02:01:37 +0600

    Add serving static files on the backend

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,6 +5,7 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { createConnection, getConnectionOptions } from "typeorm";
 import cors from "cors";
+import path from "path";
 
 import { UserResolver } from "./resolvers/UserResolver";
 import { AuthResolver } from "./resolvers/AuthResolver";
@@ -12,6 +13,7 @@ import { LadderResolver } from "./resolvers/LadderResolver";
 
 const bootstrap = async () => {
   const app = express();
+
   app.use(cors());
 
   const dbOptions = await getConnectionOptions(
@@ -26,6 +28,13 @@ const bootstrap = async () => {
     context: ({ req, res }) => ({ req, res })
   });
   apolloServer.applyMiddleware({ app });
+
+  const buildPath = path.resolve(__dirname, "../../client/build");
+  app.use(express.static(buildPath));
+
+  app.use("*", (_, res) => {
+    res.sendFile(path.resolve(__dirname, "../../client/build", "index.html"));
+  });
 
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {
